696190e4ac42280627754186d628f024ceb6c701
tyrocjh <nashcjh@163.com>
2016-08-10 22:01:12 +0800

    home page temp

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -5,6 +5,16 @@ export default class Home extends Component {
 		return (
 			<div id="home">
 				<div className="home-bg"></div>
+				<section className="about-me">
+					<div className="avatar"></div>
+					<h3>我是喵星人</h3>
+					<p>90后天蝎男，前端工程师，全栈开发尝试者</p>
+					<div className="links">
+						<a href="#"><i className="fa fa-github-alt"></i></a>
+						<a href="#"><i className="fa fa-book"></i></a>
+						<a href="#"><i className="fa fa-dribbble"></i></a>
+					</div>
+				</section>
 			</div>
 		)
 	}
