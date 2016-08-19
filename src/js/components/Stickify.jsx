eb2053729ffe6023053fc5f6a319b1eb3c6a0723
tychen <tychen@xogrp.com>
2016-08-19 18:21:23 +0800

    add sticky to blog tag

---
+++ src/js/components/Stickify.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+export default function(InnerComponent, getScrollHeight) {
+	class StickyComponent extends Component {
+		constructor(props) {
+			super(props);
+			this.state = { sticky: false };
+			this.onScroll = this.onScroll.bind(this);
+		}
+
+		componentDidMount() {
+			window.addEventListener('scroll', this.onScroll, false);
+		}
+
+		componentWillUnmount() {
+			window.addEventListener('scroll', this.onScroll, false);
+		}
+
+		onScroll() {
+			if(window.scrollY >= getScrollHeight() && !this.state.sticky) {
+				this.setState({sticky: true});
+			} else if(window.scrollY < getScrollHeight() && this.state.sticky) {
+				this.setState({sticky: false});
+			}
+		}
+
+		render() {
+			return (
+				<InnerComponent {...this.props} sticky={this.state.sticky} />
+			)
+		}
+	}
+
+	return StickyComponent;
+}
--- src/js/components/blog/BlogTags.jsx
+++ src/js/components/blog/BlogTags.jsx
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
 
-export default class BlogTags extends Component {
+import stickify from '../Stickify';
+
+class BlogTags extends Component {
 	render() {
+		const { sticky } = this.props;
+
 		return (
-			<section className="blog-tags container">
-				<a href="javascript:void(0)" className="active">全部标签</a>
-				<a href="javascript:void(0)">javascript<span>4</span></a>
-				<a href="javascript:void(0)">作品<span>12</span></a>
-				<a href="javascript:void(0)">剧中人<span>5</span></a>
-				<a href="javascript:void(0)">设计<span>6</span></a>
-				<a href="javascript:void(0)">jQuery<span>8</span></a>
-				<a href="javascript:void(0)">生活<span>15</span></a>
-				<a href="javascript:void(0)">分享<span>11</span></a>
-				<a href="javascript:void(0)">前端<span>4</span></a>
-				<a href="javascript:void(0)">工具<span>2</span></a>
+			<section className={`blog-tags ${(sticky ? 'sticky' : '')}`}>
+				<div className="container">
+					<a href="javascript:void(0)" className="active">全部标签</a>
+					<a href="javascript:void(0)">javascript<span>4</span></a>
+					<a href="javascript:void(0)">作品<span>12</span></a>
+					<a href="javascript:void(0)">剧中人<span>5</span></a>
+					<a href="javascript:void(0)">设计<span>6</span></a>
+					<a href="javascript:void(0)">jQuery<span>8</span></a>
+					<a href="javascript:void(0)">生活<span>15</span></a>
+					<a href="javascript:void(0)">分享<span>11</span></a>
+					<a href="javascript:void(0)">前端<span>4</span></a>
+					<a href="javascript:void(0)">工具<span>2</span></a>
+				</div>
 			</section>
 		)
 	}
 }
+
+function getScrollHeight() {
+	var selector = document.querySelector('.blog-page header');
+
+	return selector.offsetTop + selector.offsetHeight;
+}
+
+export default stickify(BlogTags, getScrollHeight);
